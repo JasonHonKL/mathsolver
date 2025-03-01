8fb2d6e9c7894108dc4196ca14ef346e85d9a8fc
JasonAlbertEinstien <j2004nol@gmail.com>
2025-03-01 17:48:30 +0800

    remove build folder

--- mathsolver/build/lib/core/solver.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Solver(ABC):
-    def __init__(self, provider):
-        self.provider = provider
-    
-    @abstractmethod
-    def solve(self, problem):
-        pass
--- mathsolver/build/lib/provider/__init__.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Provider(ABC):
-    @abstractmethod
-    def get_completion(self, messages, temperature=0.7, max_tokens=1000):
-        pass
